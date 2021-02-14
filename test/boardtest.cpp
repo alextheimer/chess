994134bfaded35d0e447acb3b53bd3fe57f605ff
Alex Theimer <theimer.alex@gmail.com>
2021-02-14 00:10:10 -0500

    Implemented basic Board test.

--- test/boardtest.cpp
+++ test/boardtest.cpp
@@ -1,5 +1,13 @@
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+#include "board/board.h"
 
-TEST(TestingThisClass, TestingThisFeature) {
-	ASSERT_TRUE(true);
+using namespace board;
+
+TEST(BoardTest, BasicOccupancyTest) {
+	std::unordered_map<Square, Piece> piece_map {
+		{Square(0,0), Piece(PieceType::KING, PieceColor::BLACK)}
+	};
+	Board board(piece_map);
+	ASSERT_TRUE(board.squareIsOccupied(Square(0,0)));
+	ASSERT_FALSE(board.squareIsOccupied(Square(0, 1)));
 }
