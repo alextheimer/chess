195cf2136eb97fb3b42e44b2f742d35d3448ca92
Alex Theimer <theimer.alex@gmail.com>
2021-02-15 20:14:19 -0500

    Documented math files.

--- include/util/math.h
+++ include/util/math.h
@@ -4,7 +4,19 @@
 #include <cstdint>
 
 namespace math {
+
+	/**
+	 * Returns true iff `val` is an exact power of two.
+	 *
+	 * @param val must be >= 0
+	 */
     bool isPow2(std::size_t val);
+
+    /**
+     * Returns the ceiling of the log2 function.
+     *
+     * @param val must be > 0
+     */
     std::size_t log2Ceil(std::size_t val);
 }
 
--- src/util/math.cpp
+++ src/util/math.cpp
@@ -1,17 +1,21 @@
 #include <cassert>
 #include "util/math.h"
 
+// The number of bits in size_t.
 static const std::size_t NUM_SIZE_T_BITS = sizeof(std::size_t) * 8;
 
 bool math::isPow2(std::size_t val) {
-    assert(val >= 0);
+    assert(val >= 0);  // just in case param type ever changes
     return (1 == __builtin_popcountl(val));
 }
 
 std::size_t math::log2Ceil(std::size_t val) {
+	assert(val > 0);  // undefined for val == 0
     int num_lead_zeros = __builtin_clzl(val);
     std::size_t result = NUM_SIZE_T_BITS - num_lead_zeros;
     if (isPow2(val)) {
+    	// Suppose val == 1. Then num_lead_zeros == 63, and result == 1.
+    	// Hence `result` overcounts by one unless we check for this condition.
         result -= 1;
     }
     return result;
