10e8d6269b97f5721998f7847efb2e8622e29080
Alex Theimer <theimer.alex@gmail.com>
2021-06-15 09:56:38 -0400

    Added SetBitTest.

--- test/bitopstest.cpp
+++ test/bitopstest.cpp
@@ -12,7 +12,7 @@ using namespace bitops;
 TEST(BitOpsTest, GetBitTest) {
     typedef struct TestSpec {
         BitOpType bits;
-        size_t index;
+        std::size_t index;
         bool expected;
     } TestSpec;
 
@@ -28,5 +28,33 @@ TEST(BitOpsTest, GetBitTest) {
     for (TestSpec test_spec : spec_vec) {
         sprintf(msg_buffer, "bits: %lu, index: %lu", test_spec.bits, test_spec.index);
         ASSERT_EQ(test_spec.expected, getBit(test_spec.bits, test_spec.index)) << msg_buffer;
+    }
+}
+
+TEST(BitOpsTest, SetBitTest) {
+    typedef struct TestSpec {
+        BitOpType bits;
+        std::size_t index;
+        bool expected_before;
+        bool set_bit;
+    } TestSpec;
+
+    std::vector<TestSpec> spec_vec = {
+           // bits                                  index    before    set_bit
+            { static_cast<BitOpType>(1) << 63,      0,       true,     false },
+            { static_cast<BitOpType>(1) << 63,      63,      false,    true },
+            { 1,                                    63,      true,     false },
+            { 1,                                    0,       false,    true }
+    };
+
+    char msg_buffer[100];
+    for (TestSpec test_spec : spec_vec) {
+        BitOpType bits = test_spec.bits;
+        sprintf(msg_buffer, "original bits: %lu, index: %lu, set_bit: %b",
+                test_spec.bits, test_spec.index, test_spec.set_bit);
+        // sanity check
+        ASSERT_EQ(test_spec.expected_before, getBit(bits, test_spec.index)) << msg_buffer;
+        setBit(&bits, test_spec.index, test_spec.set_bit);
+        ASSERT_EQ(test_spec.set_bit, getBit(bits, test_spec.index)) << msg_buffer;
     }
 }
