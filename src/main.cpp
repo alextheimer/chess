62511d6de91a9561cec3b0a8b1056c9f6747f6cd
Alex Theimer <theimer.alex@gmail.com>
2021-06-22 15:30:00 -0400

    Added simple test to main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,23 @@
-int main(int argc, char *argv[]) {}
+#include <unordered_map>
+#include <iostream>
+
+#include "board/board.h"
+#include "game/game.h"
+#include "player/human.h"
+
+int main(int argc, char *argv[]) {
+    board::Board board(std::unordered_map<board::Square, board::Piece>{
+        {board::Square(1, 1), (board::Piece){board::PieceType::KING, board::PieceColor::BLACK}},
+        {board::Square(5, 5), (board::Piece){board::PieceType::KING, board::PieceColor::WHITE}}
+    });
+
+    player::Human player1, player2;
+
+    game::Game game(board, player1, player2);
+
+    game.render(std::cout);
+
+    game.runPly();
+
+    game.render(std::cout);
+}
