9efe49d81d440515f582116fe419cf5e5d9062dd
Alex Theimer <theimer.alex@gmail.com>
2021-06-24 12:58:22 -0400

    Added addl tests/documentation to bitopstest.

--- test/util/bitopstest.cpp
+++ test/util/bitopstest.cpp
@@ -10,6 +10,27 @@
 
 using namespace bitops;
 
+/*
+~~~ Test Partitions ~~~
+getBit
+    index: first, last, elsewhere
+    expected: 0, 1
+setBit
+    index: first, last, elsewhere
+    original: 0, 1
+    set_bit: 0, 1
+    original/set_big: same, different
+popHighestBit
+    result: first, last, elsewhere
+    bits (after pop): 0, other
+*/
+
+/*
+Covers:
+    getBit
+        index: first, last, elsewhere
+        expected: 0, 1
+*/
 TEST(BitOpsTest, GetBitTest) {
     typedef struct TestSpec {
         BitOpType bits;
@@ -22,7 +43,9 @@ TEST(BitOpsTest, GetBitTest) {
             { static_cast<BitOpType>(1) << 63,      0,       true },
             { static_cast<BitOpType>(1) << 63,      63,      false },
             { 1,                                    63,      true },
-            { 1,                                    0,       false }
+            { 1,                                    0,       false },
+            { 7,                                    62,      true  },
+            { 0b101101,                             59,      false }
     };
 
     for (TestSpec test_spec : spec_vec) {
@@ -32,6 +55,14 @@ TEST(BitOpsTest, GetBitTest) {
     }
 }
 
+/*
+Covers:
+    setBit
+        index: first, last, elsewhere
+        original: 0, 1
+        set_bit: 0, 1
+        original/set_big: same, different
+*/
 TEST(BitOpsTest, SetBitTest) {
     typedef struct TestSpec {
         BitOpType bits;
@@ -40,12 +71,20 @@ TEST(BitOpsTest, SetBitTest) {
         bool set_bit;
     } TestSpec;
 
+    /*
+    Note: `before` is included only as a sanity check.
+    get_bit will "confirm" that we're setting the bit from an expected value.
+    */
     std::vector<TestSpec> spec_vec = {
            // bits                                  index    before    set_bit
             { static_cast<BitOpType>(1) << 63,      0,       true,     false },
             { static_cast<BitOpType>(1) << 63,      63,      false,    true },
             { 1,                                    63,      true,     false },
-            { 1,                                    0,       false,    true }
+            { 1,                                    0,       false,    true },
+            { 7,                                    62,      true,     false },
+            { 0b101101,                             59,      false,    true },
+            { 7,                                    62,      true,     true },
+            { 0b101101,                             59,      false,    false }
     };
 
     for (TestSpec test_spec : spec_vec) {
@@ -59,5 +98,33 @@ TEST(BitOpsTest, SetBitTest) {
         ASSERT_EQ(test_spec.expected_before, getBit(bits, test_spec.index)) << error_msg.str();
         setBit(&bits, test_spec.index, test_spec.set_bit);
         ASSERT_EQ(test_spec.set_bit, getBit(bits, test_spec.index)) << error_msg.str();
+    }
+}
+
+/*
+Covers:
+    popHighestBit
+        result: first, last, elsewhere
+        bits (after pop): 0, other
+*/
+TEST(BitOpsTest, PopHighestBitTest) {
+    struct TestSpec {
+        BitOpType bits;
+        BitOpType expected_bits;
+        std::size_t expected_index;
+    };
+
+    std::vector<TestSpec> spec_vec = {
+           // bits                                expected_bits    expected_index
+            { static_cast<BitOpType>(1) << 63,    0,               0 },
+            { 1,                                  0,               63 },
+            { 7,                                  3,               61 }
+    };
+
+    for (TestSpec test_spec : spec_vec) {
+        BitOpType bits = test_spec.bits;
+        ASSERT_EQ(test_spec.expected_index, popHighestBit(&bits))
+            << "bits before: " << test_spec.bits << ", after: " << bits;
+        ASSERT_EQ(test_spec.expected_bits, bits);
     }
 }
