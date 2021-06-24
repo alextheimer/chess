1c9633d1fb0a0baebf8b23a5f03d3f69254ac808
Alex Theimer <theimer.alex@gmail.com>
2021-06-24 14:55:39 -0400

    Updated piecetest documentation/tests.

--- test/board/piecetest.cpp
+++ test/board/piecetest.cpp
@@ -1,19 +1,30 @@
 #include <vector>
+#include <string>
 
 #include "gtest/gtest.h"
 #include "board/piece.h"
 
 using namespace board;
 
-TEST(PieceTest, CompressionTest) {
-    std::vector<Piece> pieces {
-        (Piece){PieceType::KING, PieceColor::BLACK},
-        (Piece){PieceType::PAWN, PieceColor::WHITE},
-        (Piece){PieceType::BISHOP, PieceColor::BLACK}
-    };
+/*
+~~~ Test Partitions ~~~
+compressPiece/decompressPiece
+    type: { all piece types }
+    color:{ all piece colors }
+*/
 
-    for (Piece piece : pieces) {
-        Piece decompressed = decompressPiece(compressPiece(piece));
-        ASSERT_EQ(piece, decompressed);
+/*
+Covers:
+    compressPiece/decompressPiece
+        type: { all piece types }
+        color:{ all piece colors }
+*/
+TEST(PieceTest, CompressDecompressTest) {
+    for (int icolor = 0; icolor < static_cast<int>(PieceColor::NUM_PIECE_COLORS); ++icolor) {
+        for (int itype = 0; itype < static_cast<int>(PieceType::NUM_PIECE_TYPES); ++itype) {
+            Piece piece = { static_cast<PieceType>(itype), static_cast<PieceColor>(icolor) };
+            Piece decoded = decompressPiece(compressPiece(piece));
+            ASSERT_EQ(piece, decoded);
+        }
     }
 }
