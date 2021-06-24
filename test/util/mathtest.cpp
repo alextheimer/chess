6075ad200a7394e6670e1e08ce0ebe7437de1d86
Alex Theimer <theimer.alex@gmail.com>
2021-06-24 13:20:49 -0400

    Added tests/documentation to mathtest.cpp.

--- test/util/mathtest.cpp
+++ test/util/mathtest.cpp
@@ -9,13 +9,31 @@
 
 using namespace math;
 
+/*
+~~~ Test Partitions ~~~
+isPow2
+    val: highest-order bit at index 0, at index 63, elsewhere
+    val: multiple bits, single bit
+log2Ceil
+    val: highest-order bit at index 0, at index 63, elsewhere
+    val: multiple bits, single bit
+*/
+
+/*
+Covers:
+    isPow2
+        val: highest-order bit at index 0, at index 63, elsewhere
+        val: multiple bits, single bit
+
+*/
 TEST(MathTest, IsPow2Test) {
     std::unordered_map<std::size_t, bool> test_map = {
            // value                              expected
             { static_cast<size_t>(1) << 63,      true },
             { static_cast<size_t>(1) << 28,      true },
             { 1,                                 true },
-            { 3,                                 false }
+            { 3,                                 false },
+            { 0x8000000000000001,                false }
     };
 
     for (auto pair : test_map) {
@@ -23,13 +41,20 @@ TEST(MathTest, IsPow2Test) {
     }
 }
 
+/*
+Covers:
+    log2Ceil
+        val: highest-order bit at index 0, at index 63, elsewhere
+        val: multiple bits, single bit
+*/
 TEST(MathTest, Log2CeilTest) {
     std::unordered_map<std::size_t, size_t> test_map = {
            // value                              expected
             { static_cast<size_t>(1) << 63,      63 },
             { static_cast<size_t>(1) << 28,      28 },
             { 1,                                 0 },
-            { 3,                                 2 }
+            { 3,                                 2 },
+            { 0x8000000000000001,                64 }
     };
 
     for (auto pair : test_map) {
