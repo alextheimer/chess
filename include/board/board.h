e919314f13ebf0b20c9ee28f9c033f880cc97a98
Alex Theimer <theimer.alex@gmail.com>
2021-06-28 10:29:55 -0400

    Added documentation to board namespace headers.

--- include/board/board.h
+++ include/board/board.h
@@ -9,114 +9,144 @@
 
 namespace board {
 
-// row/col Board index
+// datatype used for Board row/col indices
 typedef uint8_t DimIndex;
 
-// TODO(theimer): move this somewhere else?
-// "Backend" type used only in Board definition and implementation file.
+// TODO(theimer): move this to the implementation file
 // Stores a single bit of data for each of the 64 spaces on a Board.
 typedef uint64_t Bitboard;
 
-// TODO(theimer): make all structs immutable
+// TODO(theimer): make Square/Piece immutable
+
 class Square {
  public:
 
-    // would make this POD, but want to enforce constructor initialization
-    //     to support row/col assertions.
+    // this is really just a POD should-be-a-struct type, but I want to enforce
+    //     constructor initialization to support row/col assertions.
 
     DimIndex row;
     DimIndex col;
 
-    /**
-     * row and col must each lie on [0, Board::WIDTH).
-     */
+    /*
+    row and col must each lie on [0, Board::WIDTH).
+    */
     Square(DimIndex row, DimIndex col);
 
+    // TODO(theimer): why is this a member, but the Piece toString's are not?
     std::string toString() const;
-
 };
 
-// need non-member to support Square keys in maps
+// Note: need non-member to support Square map keys
 bool operator==(const Square& lhs, const Square& rhs);
-
 
 std::ostream& operator<<(std::ostream& out, const Square& move);
 
-/**
- * Maintains an 8x8 chess board.
- */
+/*
+Maintains an 8x8 chess board.
+*/
 class Board {
-    /*
-     * Each Board is maintained by a series of "Bitboards", where each Bitboard
-     * stores one bit of data for each of the 64 Board spaces.
-     *
-     * A Bitboard is stored for each PieceColor and PieceType. TODO(theimer): expand.
-     */
-
  private:
+    /*
+    Each Board is maintained by a series of "Bitboards", where each Bitboard
+    stores one bit of data for each of the 64 Board spaces.
+    
+    One Bitboard is stored for each PieceColor and PieceType.
+    */
     Bitboard piece_bitboards_[static_cast<int>(PieceType::NUM_PIECE_TYPES)] = { 0 };
     Bitboard color_bitboards_[static_cast<int>(PieceColor::NUM_PIECE_COLORS)] = { 0 };
 
  public:
+    // size of either dimension of the Board (i.e. WIDTH rows and WIDTH columns)
     static const std::size_t WIDTH = 8;
+    // total number of spaces on the Board.
     static const std::size_t SIZE = WIDTH * WIDTH;
 
+    /*
+    Constructs an empty board.
+    */
     Board();
 
-    /**
-     * Constructs a Board instance from a Square->Piece mapping.
-     * I.e. for each pair (square, piece), piece is stored at square.
-     *
-     * TODO(theimer): expand.
-     */
+    /*
+    Constructs a Board instance from a Square->Piece mapping.
+    I.e. for each pair (square, piece), `piece` is stored at `square`.
+    */
     explicit Board(const std::unordered_map<Square, Piece>& piece_map);
 
-    /**
-     * Returns true iff `square` is occupied on the Board.
-     */
+    /*
+     Returns true iff `square` is occupied on the Board.
+    */
     bool squareIsOccupied(const Square& square) const;
 
-    /**
-     * Stores the piece described by `piece` at `square` on the Board.
-     */
+    /*
+    Returns true iff `square` is occupied on the Board by a piece with the specified PieceColor.
+    */
+    bool squareIsOccupiedColor(const Square& square, PieceColor color);
+
+    // TODO(theimer): add an "overwritePiece" member
+    /*
+    Sets the piece described by `piece` at `square` on the Board.
+    */
     void setPiece(const Piece& piece, const Square& square);
 
-    // TODO(theimer): document
+    // TODO(theimer): add a "moveOverwritePiece" member
+    /*
+    Moves a piece from one square to another.
+    @param from: must be occupied
+    */
     void movePiece(Square& from, Square& to);
 
-    // TODO(theimer): document
+    /*
+    @note: might allow the implementation to return a result faster than Board::getPiece.
+    @param square: must be occupied
+    */
     PieceType getPieceType(Square& square);
 
-    // TODO(theimer): document
+    /*
+    @note: might allow the implementation to return a result faster than Board::getPiece.
+    @param square: must be occupied
+    */
     PieceColor getPieceColor(Square& square);
 
-    //TODO(theimer): document
+    /*
+    @note: use Board::getPieceType/getPieceColor if only partial Piece info is needed.
+    @param square: must be occupied
+    */
     Piece getPiece(Square& square);
 
-    // TODO(theimer): document
+    /*
+    @param square: must be occupied
+    */
     void removePiece(Square& square);
 
-    // TODO(theimer): document
+    /*
+    Fills a buffer with all Squares that contain a piece with the specified PieceColor.
+    @param buffer: an Iterator at the beginning of the buffer.
+    @return: the number of Squares added to the buffer.
+    */
     std::size_t getOccupiedSquares(PieceColor color, Square * buffer);
 
-    // TODO(theimer): document
+    /*
+    Fills a buffer with all Squares that contain a piece with the specified PieceType.
+    @param buffer: an Iterator at the beginning of the buffer.
+    @return: the number of Squares added to the buffer.
+    */
     std::size_t getOccupiedSquares(PieceType type, Square * buffer);
 
-    // TODO(theimer): document
+    /*
+    Fills a buffer with all Squares that contain a piece.
+    @param buffer: an Iterator at the beginning of the buffer.
+    @return: the number of Squares added to the buffer.
+    */
     std::size_t getOccupiedSquares(Square * buffer);
-
-    // TODO(theimer): document
-    bool squareIsOccupiedColor(const Square& square, PieceColor color);
 };
 
 }  // namespace board
 
+// these support use of these types as map keys
 namespace std {
-
 template <> struct hash<board::Square> {
     size_t operator()(const board::Square& x) const;
 };
-
 template <> struct hash<board::Piece> {
     size_t operator()(const board::Piece& x) const;
 };
--- include/board/piece.h
+++ include/board/piece.h
@@ -5,13 +5,12 @@
 
 #include "util/math.h"
 
-// TODO(theimer): move all piece compression stuff to a different namespace
-
 namespace board {
 
 enum class PieceType { KING, QUEEN, PAWN, ROOK, KNIGHT, BISHOP, NUM_PIECE_TYPES };
 enum class PieceColor { BLACK, WHITE, NUM_PIECE_COLORS };
 
+// TODO(theimer): move all piece compression stuff to a different namespace?
 typedef uint8_t CompressedPiece;
 
 typedef struct Piece {
@@ -20,16 +19,14 @@ typedef struct Piece {
 } Piece;
 
 bool operator==(const Piece& lhs, const Piece& rhs);
+std::ostream& operator<<(std::ostream& ostream, const Piece& piece);
 
 CompressedPiece compressPiece(const Piece& piece);
-
 Piece decompressPiece(CompressedPiece compressed_piece);
 
 std::string toString(PieceColor color);
 std::string toString(PieceType type);
 std::string toString(const Piece& piece);
-
-std::ostream& operator<<(std::ostream& ostream, const Piece& piece);
 
 }
 
