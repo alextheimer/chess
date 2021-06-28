d85717c4dbd892b0e06421421311dc551b7df0fd
Alex Theimer <theimer.alex@gmail.com>
2021-06-28 11:36:40 -0400

    Added documentation to bitops.cpp.

--- src/util/bitops.cpp
+++ src/util/bitops.cpp
@@ -4,22 +4,29 @@
 
 #include "util/assert.h"
 
-bool validBitIndex(std::size_t bit_index) {
+// TODO(theimer): bit index 0 should be least significant
+
+bool isValidBitIndex(std::size_t bit_index) {
     return (bit_index < util::NUM_BITOP_BITS) && (bit_index >= 0);
 }
 
 void util::setBit(BitOpType* bits, std::size_t bit_index, bool bit) {
-    ASSERT(validBitIndex(bit_index), "index: " + std::to_string(bit_index));
+    ASSERT(isValidBitIndex(bit_index), "index: " + std::to_string(bit_index));
+    // make a mask of all 1's except at the index
     std::size_t shift_count = (util::NUM_BITOP_BITS - bit_index - 1);
     BitOpType mask = ~((BitOpType)1 << shift_count);
+    // zero out the bit at the index
     *bits &= mask;
+    // "or" in the new bit
     *bits |= (static_cast<BitOpType>(bit) << shift_count);
 }
 
 bool util::getBit(const BitOpType bits, size_t bit_index) {
-    ASSERT(validBitIndex(bit_index), "index: " + std::to_string(bit_index));
+    ASSERT(isValidBitIndex(bit_index), "index: " + std::to_string(bit_index));
+    // make a mast of all 0's except at the index
     std::size_t shift_count = (util::NUM_BITOP_BITS - bit_index - 1);
     BitOpType mask = (BitOpType)1 << shift_count;
+    // "and" it with the bits, then shift it to the least-significant index
     BitOpType bitVal = (bits & mask) >> shift_count;
     ASSERT((bitVal == 0) || (bitVal == 1), "bitVal: " + std::to_string(bitVal));
     return static_cast<bool>(bitVal);
