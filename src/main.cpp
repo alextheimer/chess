e5d3d78c8de797902e3c3efbc0c568b74fed1d19
Alex Theimer <theimer.alex@gmail.com>
2021-06-29 17:16:16 -0400

    Updated main.cpp to reflect updates elsewhere.

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,14 @@
 
 int main(int argc, char *argv[]) {
     board::Board board(game::INIT_PIECE_MAP);
+    player::Human player1("Jimbo Slice"), player2("Jenny Craig");
+    game::Game game(&board, &player1, &player2);
 
-    player::Human player1, player2;
+    while (!game.isEnded()) {
+        game.renderBoard(std::cout);
+        game.runPly();
+    }
 
-    game::Game game(board, player1, player2);
-
-    game.renderBoard(std::cout);
-
-    game.runPly();
-
-    game.renderBoard(std::cout);
+    player::Human& winner = dynamic_cast<player::Human&>(game.getWinner());
+    std::cout << winner.getName() << " wins!";
 }
