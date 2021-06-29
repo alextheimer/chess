e85eb7a667acf571f0c7fec632be652dd9ef3f80
Alex Theimer <theimer.alex@gmail.com>
2021-06-29 15:20:53 -0400

    Minor Human Player prompt update.

--- src/player/human.cpp
+++ src/player/human.cpp
@@ -10,8 +10,8 @@ using board::Square;
 using game::Move;
 
 Move player::Human::getMove(const Board& board) {
-    std::size_t row1, col1, row2, col2;
-    std::cout << "Enter row1 col1 row2 col2: ";
-    std::cin >> row1 >> col1 >> row2 >> col2;
-    return (Move){ Square(row1, col1), Square(row2, col2) };
+    std::size_t row_from, col_from, row_to, col_to;
+    std::cout << "Enter row_from col_from row_to col_to: ";
+    std::cin >> row_from >> col_from >> row_to >> col_to;
+    return (Move){ Square(row_from, col_from), Square(row_to, col_to) };
 }
