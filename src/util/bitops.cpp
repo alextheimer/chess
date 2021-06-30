5782b921398f8ac0a1a18a95bee6f324223d68b0
Alex Theimer <theimer.alex@gmail.com>
2021-06-30 18:35:04 -0400

    Optimized setBit and getBit.

--- src/util/bitops.cpp
+++ src/util/bitops.cpp
@@ -12,20 +12,16 @@ bool isValidBitIndex(std::size_t bit_index) {
 
 void util::setBit(BitOpType* bits, std::size_t bit_index, bool bit) {
     ASSERT(isValidBitIndex(bit_index), "index: " + std::to_string(bit_index));
-    // make a mask of all 1's except at the index
-    BitOpType mask = ~((BitOpType)1 << bit_index);
-    // zero out the bit at the index
-    *bits &= mask;
-    // "or" in the new bit
-    *bits |= (static_cast<BitOpType>(bit) << bit_index);
+    // Courtesy of https://stackoverflow.com/a/47990.
+    // This eliminates one bit-shift from a naive implementation and makes
+    //     it surprisingly less of a bottleneck.
+    *bits ^= (-static_cast<BitOpType>(bit) ^ *bits)
+             & (static_cast<BitOpType>(1) << bit_index);
 }
 
 bool util::getBit(BitOpType bits, std::size_t bit_index) {
     ASSERT(isValidBitIndex(bit_index), "index: " + std::to_string(bit_index));
-    // make a mast of all 0's except at the index
-    BitOpType mask = (BitOpType)1 << bit_index;
-    // "and" it with the bits, then shift it to the least-significant index
-    BitOpType bitVal = (bits & mask) >> bit_index;
+    BitOpType bitVal = (bits >> bit_index) & static_cast<BitOpType>(1);
     ASSERT((bitVal == 0) || (bitVal == 1), "bitVal: " + std::to_string(bitVal));
     return static_cast<bool>(bitVal);
 }
