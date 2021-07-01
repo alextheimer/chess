8f4146ee6bdeab9b3750fd405de2aa569a2f8ddc
Alex Theimer <theimer.alex@gmail.com>
2021-06-30 20:14:34 -0400

    Added random seed and Computers to main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,27 @@
 
 #include <unordered_map>
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 #include "board/board.h"
 #include "game/game.h"
 #include "player/human.h"
+#include "player/computer/computer.h"
 #include "util/assert.h"
 
 int main(int argc, char *argv[]) {
     board::Board board(game::INIT_PIECE_MAP);
-    player::Human player1("Jimbo Slice"), player2("Jenny Craig");
+    player::Computer player1("RoboJim9000"), player2("RoboTim9000");
     game::Game game(&board, &player1, &player2);
+
+    std::srand(std::time(NULL));
 
     while (!game.isEnded()) {
         game.renderBoard(std::cout);
         game.runPly();
     }
 
-    player::Human& winner = dynamic_cast<player::Human&>(game.getWinner());
+    game::Player& winner = game.getWinner();
     std::cout << winner.getName() << " wins!";
 }
