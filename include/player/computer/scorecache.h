36818f5256b689a3371cd69fb39aac3a07f8372b
Alex Theimer <theimer.alex@gmail.com>
2021-07-06 13:31:43 -0400

    Added documentation to ScoreCache.

--- include/player/computer/scorecache.h
+++ include/player/computer/scorecache.h
@@ -10,16 +10,37 @@ namespace computer{
 
 typedef int64_t BoardScore;
 
+// TODO(theimer): make IScoreCache. Possibly change Player, as well.
+
 /*
-Data structure that allows fast storage/retrieval of BoardScores.
-TODO(theimer): expand
+Interface of a data structure that allows fast storage/retrieval of BoardScores.
+BoardScores are keyed on Board-depth pairs.
 */
 class ScoreCache {
  public:
+    /*
+    Returns an iterator referring to the "past-the-end" BoardScore.
+
+    The iterator doesn't point to any BoardScore and should not be dereferenced.
+    */
     virtual BoardScore* end() const  = 0;
+
+    /*
+    Returns a pointer to the BoardScore if the Board-depth pair
+    exists in the map.
+
+    Otherwise, returns the end iterator (i.e. the return value of end()).
+
+    @param depth: must be >= 0
+    */
     virtual BoardScore* find(const board::Board& board,
                              std::size_t depth) const = 0;
-    virtual void set(const board::Board& board, std::size_t key,
+    /*
+    Sets the BoardScore at the Board-depth pair.
+
+    @param depth: must be >= 0
+    */
+    virtual void set(const board::Board& board, std::size_t depth,
                      BoardScore value) = 0;
 };
 
